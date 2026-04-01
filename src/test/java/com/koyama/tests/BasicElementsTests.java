fbf2b8f832d53965a30161bf776bd3f423f3c749
Matheus Koyama <koyamaCRS@gmail.com>
2026-03-31 22:07:26 -0300

    adiciona novos cenários de automação e melhorias nos testes

--- src/test/java/com/koyama/tests/BasicElementsTests.java
+++ src/test/java/com/koyama/tests/BasicElementsTests.java
@@ -1,12 +1,21 @@
 package com.koyama.tests;
 
-import java.time.Duration;
+import static org.testng.Assert.assertEquals;
 
+import java.time.Duration;
+import java.util.Iterator;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
 
 public class BasicElementsTests {
 
@@ -19,6 +28,89 @@ public class BasicElementsTests {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    
+    
+    @Test
+    public void shouldOpenABTestingPageAndDisplayParagraph(){
+    	
+        driver.get("https://the-internet.herokuapp.com/");
+
+        By linkABTesting  = By.linkText("A/B Testing");
+        By paragrafoABTesting  = By.xpath("//div[@id='content']//div[@class='example']/p");
+        
+        wait.until(ExpectedConditions.elementToBeClickable(linkABTesting)).click();
+        wait.until(ExpectedConditions.urlContains("/abtest"));
+        
+        
+        WebElement textoParagrafo = wait.until(
+        		ExpectedConditions.visibilityOfElementLocated(paragrafoABTesting)
+        		);
+        
+        System.out.print(textoParagrafo.getText());     
+              
+    }
+    
+    @Test
+    public void shouldAddMultipleElementsSuccessfully() throws InterruptedException{
+    	
+      	driver.get("https://the-internet.herokuapp.com/");
+      	
+      	By linkAddRemoveElements = By.linkText("Add/Remove Elements");
+      	By buttonaAdd = By.cssSelector("button[onclick='addElement()']");
+      	
+      	wait.until(ExpectedConditions.elementToBeClickable(linkAddRemoveElements)).click();
+      	wait.until(ExpectedConditions.urlContains("/add_remove_elements/"));
+      	
+      	int n = 5;
+      	
+      	
+      	for(int i = 0; i <= n; i++) {
+      		wait.until(ExpectedConditions.elementToBeClickable(buttonaAdd)).click();
+      		Thread.sleep(1500);
+      	}
+      	  
+    }
+    
+    @Test
+    public void shouldOpenBrokenImagesPageSuccessfully(){
+    	
+      	driver.get("https://the-internet.herokuapp.com/");
+
+      	By brokenImages = By.linkText("Broken Images");
+      	By textBroken = By.cssSelector("");
+      	
+      	
+      	wait.until(ExpectedConditions.elementToBeClickable(brokenImages)).click();
+        wait.until(ExpectedConditions.urlContains("/broken_images"));
+        
+
+
+    }
+    
+    @Test
+    public void shouldAddMultipleElementsChallengingDOM()  throws InterruptedException {
+      	  	   
+    	   driver.get("https://the-internet.herokuapp.com/");
+       By challenging = By.linkText("Challenging DOM");
+       By buttonqux = By.cssSelector("a.button.success");
+       By text = By.cssSelector("p");
+       
+       wait.until(ExpectedConditions.elementToBeClickable(challenging)).click();
+       wait.until(ExpectedConditions.urlContains("/challenging_dom"));
+       
+       int n = 5;
+       
+       for(int i = 0; i < n; i++) {
+    	      wait.until(ExpectedConditions.elementToBeClickable(buttonqux)).click();
+    	      Thread.sleep(1500);
+       }
+                       
+       String texto = "The hardest part in automated web testing is finding the best locators (e.g., ones that well named, unique, and unlikely to change). It's more often than not that the application you're testing was not built with this concept in mind. This example demonstrates that with unique IDs, a table with no helpful locators, and a canvas element.";
+       WebElement p = wait.until(ExpectedConditions.visibilityOfElementLocated(text));
+       
+        Assert.assertEquals(texto, p.getText());
+       
     }
 
     @AfterMethod
